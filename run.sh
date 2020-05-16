ea52c54b4f98fd422fd791a2b29855cac966cc7b
Huynh Hoang Anh <anh.huynh@codestringers.com>
2020-05-17 00:34:26 +0700

    execute file

---
+++ run.sh
@@ -0,0 +1,10 @@
+
+export DB_PASSWORD=F0b3life
+export DB_USERNAME=root
+export FOBELIFE_PAYMENT_CANCEL=http://vip.fobelife.com/nganluong/cancel
+export FOBELIFE_PAYMENT_RETURN=http://vip.fobelife.com/nganluong/return
+export PAYMENT_SECURE_PASS=2ced9dd0153f74c7f01529375ec93e06
+export PAYMENT_SITE_CODE=48741
+export PAYMENT_URL=https://sandbox.nganluong.vn:8088/nl35/checkout.php
+
+java -jar ./target/fobelife.war
