3d5cd3ff7d8418742e112c1c115879cb5ab6cf33
hoanganhf1 <hoanganhf1@gmail.com>
2020-07-01 03:41:46 +0700

    run pro

--- run.sh
+++ run.sh
@@ -6,5 +6,9 @@ export FOBELIFE_PAYMENT_RETURN=http://vip.fobelife.com/nganluong/return
 export PAYMENT_SECURE_PASS=2ced9dd0153f74c7f01529375ec93e06
 export PAYMENT_SITE_CODE=48741
 export PAYMENT_URL=https://sandbox.nganluong.vn:8088/nl35/checkout.php
+export ALEPAY_CHECKSUM=BzYokbZcVkxXhaNkU1x0kWzrFYlThO
+export ALEPAY_ENCRYPT=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCjTMboPUjtbE2t0Z0qsHfoJGN++fz/pNT8k2NFwCtFBUkgm1fsPW0FhpD2ompFQFQtmWtuftqrW/vE+vl6umhrO8nS4iyH51S2AWq5n9DqeQjeJDM46pBh0RYtn0ZsHEEgCZm0h96CTKBW465wbbJ4/gocHtS0ux/sd1KGVStIbQIDAQAB
+export ALEPAY_TOKEN=CWCQsaGIo1PnV2iGIxo3rptk7zcdcP
+export ALEPAY_URL=https://alepay-sandbox.nganluong.vn
 
 java -jar ./target/fobelife.war
